be4a35f6892f22698d136818f3c6ac1bacbf098f
Nick Ross <nick.ir.ross@gmail.com>
2017-08-19 20:46:24 +0100

    Add auth

--- luckybreak/users/models.py
+++ luckybreak/users/models.py
@@ -5,6 +5,15 @@ from django.utils.encoding import python_2_unicode_compatible
 
 @python_2_unicode_compatible
 class User(AbstractUser):
+    USER_TYPE_GUEST = 1
+    USER_TYPE_PROVIDER = 2
+    _USER_TYPE_CHOICES = (
+        (USER_TYPE_GUEST, 'Guest'),
+        (USER_TYPE_PROVIDER, 'Provider'),
+    )
+    user_type = models.IntegerField(
+        choices=_USER_TYPE_CHOICES
+    )
     phone = models.CharField(
         max_length=100, null=True, blank=True
     )
