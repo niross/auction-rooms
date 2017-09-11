ddbf514053e0b8225e41ac4bebca1548b399fa05
Nick Ross <nick.ir.ross@gmail.com>
2017-09-11 21:43:09 +0100

    Add new create experience tests

--- luckybreak/common/mixins.py
+++ luckybreak/common/mixins.py
@@ -1,16 +1,19 @@
-from detective.auctions.models import AuctionSite, FeaturedCategory
-from detective.forces.models import PoliceForce
+from django.contrib.auth.mixins import UserPassesTestMixin
+
+from luckybreak.users.models import User
 
 
-class NavContextMixin(object):
-    def get_context_data(self, **kwargs):
-        context = super(NavContextMixin, self).get_context_data(**kwargs)
-        context.update({
-            'forces': PoliceForce.objects.live(),
-            'auction_sites': AuctionSite.objects.live(),
-            'categories': FeaturedCategory.objects.live(),
-            'featured_categories': FeaturedCategory.objects.live().filter(
-                show_on_homepage=True
-            ),
-        })
-        return context
+class UserIsGuestMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.is_authenticated() \
+                and self.request.user.user_type == User.USER_TYPE_GUEST
+
+
+class UserIsProviderMixin(UserPassesTestMixin):
+    raise_exception = True
+
+    def test_func(self):
+        return self.request.user.is_authenticated() \
+                and self.request.user.user_type == User.USER_TYPE_PROVIDER
