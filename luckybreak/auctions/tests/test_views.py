6ced2ba61e632edb96d0c1acc84bfec06795a3d9
Nick Ross <nick.ir.ross@gmail.com>
2017-10-04 17:21:44 +0100

    Add basic provider auction view test

--- luckybreak/auctions/tests/test_views.py
+++ luckybreak/auctions/tests/test_views.py
@@ -31,3 +31,23 @@ class ProviderFinishedAuctionsTestCase(BaseTestCase):
         self.client.force_login(self.provider)
         self.get('auctions:provider-finished-auctions')
         self.response_200()
+
+
+class ProviderAuctionDetailTestCase(BaseTestCase):
+    fixtures = [
+        'users.json', 'currencies.json', 'experiences.json', 'auctions.json'
+    ]
+
+    def test_unauthenticated_get(self):
+        self.get('auctions:provider-auction', pk=1)
+        self.response_403()
+
+    def test_guest_get(self):
+        self.client.force_login(self.guest)
+        self.get('auctions:provider-auction', pk=1)
+        self.response_403()
+
+    def test_provider_get(self):
+        self.client.force_login(self.provider)
+        self.get('auctions:provider-auction', pk=1)
+        self.response_200()
