10c47ce4161dc1e31a4a1138b36e311898a040dd
Nick Ross <nick.ir.ross@gmail.com>
2017-10-09 16:49:38 +0100

    Add event log entries via signals

--- luckybreak/event_log/models.py
+++ luckybreak/event_log/models.py
@@ -9,32 +9,64 @@ from django.contrib.postgres.fields import JSONField
 
 
 class EventLogManager(models.Manager):
-    def log_experience_created(self, user, experience):
+    def log_experience_created(self, experience):
+        """
+        User created an experience
+        :param experience:
+        :return:
+        """
         return self.create(
-            user=user,
+            user=experience.user,
             type=EventLog.EVENT_TYPE_CREATE_EXPERIENCE,
             content_object=experience
         )
 
-    def log_auction_created(self, user, auction):
+    def log_auction_created(self, auction):
+        """
+        User created an auction
+        :param auction:
+        :return:
+        """
         return self.create(
-            user=user,
+            user=auction.experience.user,
             type=EventLog.EVENT_TYPE_CREATE_AUCTION,
             content_object=auction
         )
 
-    def log_bid_placed(self, user, bid):
+    def log_bid_placed(self, bid):
+        """
+        A bid was placed on an auction
+        :param bid:
+        :return:
+        """
         return self.create(
-            user=user,
+            user=bid.auction.experience.user,
             type=EventLog.EVENT_TYPE_BID_PLACED,
             content_object=bid
         )
 
-    def log_auction_ended(self, user, auction):
+    def log_auction_ended(self, auction):
+        """
+        An auction ended
+        :param auction:
+        :return:
+        """
         return self.create(
-            user=user,
+            user=auction.experience.user,
             type=EventLog.EVENT_TYPE_AUCTION_ENDED,
             content_object=auction
+        )
+
+    def log_placed_bid(self, bid):
+        """
+        User placed a bid on an auction
+        :param bid:
+        :return:
+        """
+        return self.create(
+            user=bid.user,
+            type=EventLog.EVENT_TYPE_PLACED_BID,
+            content_object=bid
         )
 
 
@@ -43,12 +75,14 @@ class EventLog(TimeStampedModel):
     EVENT_TYPE_CREATE_AUCTION = 2
     EVENT_TYPE_BID_PLACED = 3
     EVENT_TYPE_AUCTION_ENDED = 4
+    EVENT_TYPE_PLACED_BID = 5
 
     _EVENT_TYPE_CHOICES = (
         (EVENT_TYPE_CREATE_EXPERIENCE, 'A new experience was created'),
         (EVENT_TYPE_CREATE_AUCTION, 'A new auction was created'),
         (EVENT_TYPE_BID_PLACED, 'A bid was placed on an auction'),
         (EVENT_TYPE_AUCTION_ENDED, 'Auction finished'),
+        (EVENT_TYPE_PLACED_BID, 'User placed a bid on an auction'),
     )
     user = models.ForeignKey('users.User', related_name='events')
     type = models.IntegerField(choices=_EVENT_TYPE_CHOICES)
@@ -66,6 +100,12 @@ class EventLog(TimeStampedModel):
 
     class Meta:
         ordering = ('-created',)
+
+    def __unicode__(self):
+        return self.get_log_message()
+
+    def __str__(self):
+        return self.__unicode__()
 
     def get_log_message(self):
         if self.type == self.EVENT_TYPE_CREATE_EXPERIENCE:
@@ -87,16 +127,23 @@ class EventLog(TimeStampedModel):
             )
 
         if self.type == self.EVENT_TYPE_BID_PLACED:
-            return 'A bid for {} was received on auction, ' \
+            return 'A bid for {} was received on auction ' \
                '<a href="{}">{}</a>.'.format(
-                    self.content_object.auction.formatted_current_price(),
+                    self.content_object.formatted_price(),
                     self.content_object.auction.get_provider_absolute_url(),
                     self.content_object.auction.title
                 )
 
         if self.type == self.EVENT_TYPE_AUCTION_ENDED:
             return 'Auction <a href="{}">{}</a> finished.'.format(
-                    self.content_object.get_provider_absolute_url(),
-                    self.content_object.title
+                self.content_object.get_provider_absolute_url(),
+                self.content_object.title
             )
 
+        if self.type == self.EVENT_TYPE_PLACED_BID:
+            return 'You placed a bid for {} on auction ' \
+               '<a href="{}">{}</a>.'.format(
+                    self.content_object.formatted_price(),
+                    self.content_object.auction.get_guest_absolute_url(),
+                    self.content_object.auction.title
+                )
