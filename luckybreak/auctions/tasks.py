54fabd580e4770c9635592c2bb293c2a1a313ebf
Nick Ross <nick.ir.ross@gmail.com>
2017-10-16 06:45:49 +0100

    Increment search appearace count on models

--- luckybreak/auctions/tasks.py
+++ luckybreak/auctions/tasks.py
@@ -2,6 +2,7 @@ import logging
 from datetime import datetime
 
 from celery.task import task
+from django.db.models import F
 
 from luckybreak.auctions.models import Auction
 
@@ -26,3 +27,17 @@ def complete_auctions():
         print('Marking auction \'%s\' as complete', auction.id)
         log.debug('Marking auction \'%s\' as complete', auction.id)
         auction.mark_complete()
+
+
+@task
+def increment_search_appearance_count(auction_ids):
+    """
+    Add 1 to search the search_appearance_count field for
+    all auction ids provided
+
+    :param auction_ids:
+    :return:
+    """
+    Auction.objects.filter(id__in=auction_ids).update(
+        search_appearance_count=F('search_appearance_count') + 1
+    )
