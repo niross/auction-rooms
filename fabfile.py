c36a11ec67779813b4981b59ae42e91f4534db07
Nick Ross <nick.ir.ross@gmail.com>
2017-10-25 07:26:02 +0100

    Use webpack fileloader

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,33 @@
+import os
+
+from datetime import datetime
 from fabric.api import runs_once, lcd, local, task
+from fabric.context_managers import cd, shell_env
+from fabric.operations import run, sudo, get
+from fabric.state import env
+
+USER = os.environ['USER']
+SITE_ROOT = '/var/www/sites/luckybreak'
+DJANGO_MANAGE = os.path.join(SITE_ROOT, 'manage.py')
+VENV_ACTIVATE = '/home/nick/.virtualenvs/luckybreak/bin/activate'
+DB_PATH = os.path.join(SITE_ROOT, 'db/lander.sqlite3')
+
+env.hosts = 'stringer'
+env.port = 1004
+
+DJANGO_ENV = {
+    'DJANGO_SECRET_KEY': 'XXX',
+    'DJANGO_AWS_ACCESS_KEY_ID': 'xxx',
+    'DJANGO_AWS_SECRET_ACCESS_KEY': 'xxx',
+    'DJANGO_MAILGUN_API_KEY': 'xxx',
+    'DJANGO_ADMIN_URL': 'xxx',
+    'MAILGUN_SENDER_DOMAIN': 'xxx',
+    'DATABASE_URL': 'postgis://luckybreak:7YFzZ4dZ4pR@localhost/luckybreak'
+}
+
+
+def virtualenv(command):
+    run('source {} && {}'.format(VENV_ACTIVATE, command))
 
 
 @task
@@ -15,3 +44,53 @@ def register_deployment(git_path):
             ' -d rev="{}"'
             ' -d branch="{}"'
             ' -d status=completed'.format(revision, branch))
+
+
+@task
+def deploy():
+    with(cd(SITE_ROOT)):
+        with shell_env(**DJANGO_ENV):
+            run('git reset --hard')
+            run('git checkout master')
+            run('git pull origin master')
+            virtualenv('pip install -r requirements/production.txt')
+            virtualenv(
+                'python manage.py migrate --settings config.settings.production'
+            )
+            virtualenv(
+                'python manage.py collectstatic --noinput '
+                '--settings config.settings.production'
+            )
+            sudo('service apache2 reload')
+            # sudo('supervisorctl restart detective_celery')
+
+    register_deployment(os.path.dirname(os.path.realpath(__file__)))
+
+
+def import_prod_db():
+    """
+    Get a copy of the production database and import it locally
+    """
+    now = datetime.now().strftime('%Y-%m-%d')
+    filename = 'luckybreak-prod-db-backup-{}'.format(now)
+    sqlpath = os.path.join('/tmp', '{}.sql'.format(filename))
+    tarpath = os.path.join('/tmp', '{}.tgz'.format(filename))
+    backup_dir = os.path.join(os.path.dirname(__file__), 'db-backups')
+
+    local_tar = os.path.join(backup_dir, filename + '.tgz')
+    local_sql = os.path.join(backup_dir, filename + '.sql')
+
+    run('pg_dump -U luckybreak luckybreak > {}'.format(sqlpath))
+    run('tar -czvf {} -C /tmp {}'.format(tarpath, '{}.sql'.format(filename)))
+    get(tarpath, local_path=local_tar)
+    run('rm {} {}'.format(sqlpath, tarpath))
+
+    local('tar -xzf {} -C {}'.format(local_tar, backup_dir))
+    local('docker-compose -f /home/nick/projects/luckybreak/dev.yml stop')
+    local('docker-compose -f /home/nick/projects/luckybreak/dev.yml up -d postgres')
+    local('psql -p 5432 -h 127.0.0.1 -U postgres -c \'drop database luckybreak\'')
+    local('psql -p 5432 -h 127.0.0.1 -U postgres -c \'create database luckybreak\'')
+    local('psql -p 5432 -h 127.0.0.1 -U postgres -d luckybreak < {}'.format(local_sql))
+    local('rm {}'.format(local_sql))
+    local('docker-compose -f /home/nick/projects/luckybreak/dev.yml up -d')
+    local('python /home/nick/projects/luckybreak/manage.py scramble_user_emails')
