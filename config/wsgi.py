7845157e34310ea2493ebd4b88ac66af37f13ba5
Nick Ross <nick.ir.ross@gmail.com>
2017-10-25 21:02:46 +0100

    Close celery prod issues

--- config/wsgi.py
+++ config/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for binday project.
+WSGI config for luckbreak project.
 
 This module contains the WSGI application used by Django's development server
 and any production WSGI deployments. It should expose a module-level variable
