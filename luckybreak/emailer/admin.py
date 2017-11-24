68357dbfd6f236745b869e4f78a402fc89279161
Nick Ross <nick.ir.ross@gmail.com>
2017-11-24 12:58:29 +0000

    Add an admin page for email logs

--- luckybreak/emailer/admin.py
+++ luckybreak/emailer/admin.py
@@ -1,6 +1,20 @@
 # -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
-# Register your models here.
+from .models import EmailLog
+
+
+class EmailLogAdmin(admin.ModelAdmin):
+    list_display = ('name', 'recipient', 'subject', 'created')
+    fields = ('name', 'recipient', 'created', 'subject', 'content_as_html')
+    readonly_fields = (
+        'name', 'recipient', 'created', 'subject', 'content_as_html'
+    )
+
+    def content_as_html(self, obj):
+        return mark_safe(obj.content)
+    content_as_html.short_description = 'Content'
+
+
+admin.site.register(EmailLog, EmailLogAdmin)
